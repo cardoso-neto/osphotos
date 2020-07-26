81dfb402316ead4cf0b25908778b299fc4892c02
Nei Cardoso Neto <nei.neto@hotmail.com>
2020-07-26 00:20:07 -0300

    Applying KISS principle

--- design-docs/data-model.py
+++ design-docs/data-model.py
@@ -1,50 +1,44 @@
 # from enum import Enum, unique
 from datetime import date, datetime
-from typing import List, TypedDict
+from typing import List, Optional, TypedDict
 
 
+class Point:
+    x: int
+    y: int
+
+        
 class BBox:
-    x1: int
-    y1: int
-    x2: int
-    y2: int
-
-
-Emotion = str
-
-class Face(TypedDict):
-    manual: bool  # is it manually added/reviewed? 
-    emotion: Emotion
-    position: BBox
+    upper_left: Point
+    bottom_right: Point
 
 
 class Person(TypedDict):
     _id: str  # hash(name, birthday)
     name: str
-    birthday: date
+    birthday: Optional[date]
 
 
-persons: Dict[person_id, Person]
-
-
-class PersonInPicture(TypedDict):
+class PersonInPicture(TypedDict, total=False):
     person_id: str
-    position: List[Points]
-
+    face_position: Optional[BBox]
+    body_position: Optional[List[Points]]
+        
 
 gps = Tuple[float, float]
 
-
-class CustomAttributes:  # tags?
-    pass
 
 class Picture(TypedDict):
     _id: hash_digest
     creation_date: datetime
     coordinates: gps
-    tags: Set[str]
+    tags: dict
     Faces: List[BBox]
-    people: List[person_id]
+    people: List[PersonInPicture]
 
+        
+# indices
+
+persons: Dict[person_id, Person]
 
 pictures_to_files: Dict[picture_id, filepath]
